602688cfdaedca5b3e38f73703b3f9ee93e98a89
j-davidwhite <107911100+j-davidwhite@users.noreply.github.com>
2024-10-18 21:38:23 +1100

    About page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ThemeProvider, useTheme } from "./components/ThemeContext"; // Ensure you import useTheme
+import { ThemeProvider, useTheme } from "./components/ThemeContext";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Dashboard from "./pages/Dashboard";
@@ -9,30 +9,22 @@ import Analytics from "./pages/Analytics";
 import About from "./pages/About";
 
 function AppContent() {
-  const { isLightMode } = useTheme(); // Use theme within a component inside ThemeProvider
+  const { isLightMode } = useTheme();
 
   return (
     <div className={`App ${isLightMode ? "" : "dark"}`}>
-      <Sidebar>Sidebar</Sidebar>
-      <Header>Header</Header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Joshua White: 102363543, Aaron Hussain, Dheeman Thakar
-        </a>
-      </header>
-      <main>
+      {/* Keeping Sidebar and Header static */}
+      <Sidebar className="sidebar" />
+      <Header className="header" />
+
+      {/* Creating a container for the dynamic content */}
+      <div className="main-content">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/analytics" element={<Analytics />} />
           <Route path="/about" element={<About />} />
         </Routes>
-      </main>
+      </div>
     </div>
   );
 }
@@ -41,7 +33,7 @@ function App() {
   return (
     <ThemeProvider>
       <Router>
-        <AppContent /> {/* Move the content inside a component */}
+        <AppContent />
       </Router>
     </ThemeProvider>
   );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,64 @@
 import React from "react";
+import "./About.css";
+import photo1 from "../assets/joshua.memoji.png";
+import photo2 from "../assets/dheeman.memoji.png";
+import photo3 from "../assets/aaron.memoji.png";
+import instagram from "../assets/instagram.png";
+import linkedin from "../assets/linkedin.png";
+import github from "../assets/github.png";
 
 const About = () => {
-  return <h1></h1>;
+  return (
+    <div className="about">
+      <div>Our Team</div>
+      <div className="profile-container">
+        <div className="profile">
+          <div className="circle">
+            <img className="photo" src={photo2} />
+          </div>
+
+          <div className="name">name</div>
+          <div className="role">web developer</div>
+
+          <div className="icons">
+            <img src={instagram}></img>
+            <img src={github}></img>
+            <img src={linkedin}></img>
+          </div>
+        </div>
+        <div className="profile">
+          <div className="circle">
+            <img className="photo" src={photo2} />
+          </div>
+
+          <div className="name">name</div>
+          <div className="role">web developer</div>
+
+          <div className="icons">
+            <img src={instagram}></img>
+            <img src={github}></img>
+            <img src={linkedin}></img>
+          </div>
+        </div>
+        <div className="profile">
+          <div className="circle">
+            <img className="photo" src={photo2} />
+          </div>
+
+          <div className="name">name</div>
+          <div className="role">web developer</div>
+
+          <div className="icons">
+            <img src={instagram}></img>
+            <img src={github}></img>
+            <img src={linkedin}></img>
+          </div>
+        </div>
+      </div>
+
+      <div>Oreo'997</div>
+    </div>
+  );
 };
 
 export default About;
