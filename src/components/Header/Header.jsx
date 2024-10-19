8a3981f8de680dce274aa7a8dac4a92096c9ce9c
j-davidwhite <107911100+j-davidwhite@users.noreply.github.com>
2024-10-19 13:49:34 +1100

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,31 @@ import TextField from "@mui/material/TextField";
 import SettingsSharpIcon from "@mui/icons-material/SettingsSharp";
 import NotificationsSharpIcon from "@mui/icons-material/NotificationsSharp";
 import { useTheme } from "../ThemeContext.js";
+import { useLocation } from "react-router-dom";
 
 function Header() {
   const { isLightMode } = useTheme();
+  const location = useLocation();
+
+  const getPageName = () => {
+    switch (location.pathname) {
+      case "/":
+        return "Dashboard";
+      case "/Analytics":
+        return "Analytics";
+      case "/About":
+        return "About";
+      case "/Support":
+        return "Support";
+      default:
+        return "Dashboard";
+    }
+  };
 
   return (
     <div className={`header ${isLightMode ? "" : "dark"}`}>
       <div className="header-left">
-        <div className="dashboard">Dashboard</div>
+        <div className="dashboard">{getPageName()}</div>
         <div className="search-bar-container">
           <TextField
             className="search-bar" // Ensure the className prop is applied
